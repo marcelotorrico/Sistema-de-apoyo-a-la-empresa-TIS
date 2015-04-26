c7b62f1d4584dbd32bae4bfb5ce0773bde821d6f
Fernando Gutierrez <fernandogutierrez279@gmail.com>
2015-04-26 09:37:58 -0400

    Quite las condiciones if ahora se maneja en el procedimiento insert_gestion

--- Vista/crear_gestion.php
+++ Vista/crear_gestion.php
@@ -14,25 +14,7 @@ $addRol = $_POST['rol'];
     
     $peticion = $conectar->consultaProcedimiento("call insert_gestion('$addRol','$addini','$addfin')") ;
     $result=$peticion['errno'];
-  
-
-    if($peticion['errno'] == 1){
-      echo "<script type=\"text/javascript\">alert('Se registro satisfactoriamenta la gestion'); window.location='add_gestion.php';</script>";            
-    }
-    else      
-    if($peticion['errno'] == 0)
-    {
-        echo "<script type=\"text/javascript\">alert('La fecha de inicio no puede ser mayor a la de fin'); window.location='add_gestion.php';</script>";
-       
-    }
-    else 
-     if($peticion['errno'] == 2)
-     {
-        echo "<script type=\"text/javascript\">alert('Las gestiones deben ser posteriores a la fecha actual'); window.location='add_gestion.php';</script>";
-       
-     }   
-    else{
-        echo "<script type=\"text/javascript\">alert('se produjo un error'); window.location='add_gestion.php';</script>";
-    }
-
+    
+      echo "<script>alert('$result'); window.location='add_gestion.php';</script>";            
+    
 ?>
