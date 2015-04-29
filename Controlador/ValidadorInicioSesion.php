ff0edb59b06beb206169862cbd3571294be50882
anghy12 <andycotvy@gmail.com>
2015-04-29 19:27:43 -0400

    Modificado el mensaje de validacion de inicio de sesion

--- Controlador/ValidadorInicioSesion.php
+++ Controlador/ValidadorInicioSesion.php
@@ -5,6 +5,7 @@
         include_once '../Modelo/conexion.php';
          $conectar = new conexion();
          $user = isset($sesion);
+         $mensaje="Usted intento ingresar a una pagina que no le corresponde";
  
          if($user == ''){
              
@@ -14,14 +15,14 @@
             if($rol == "administrador"){
                 $consulta =$conectar->consulta("select * from administrador where NOMBRE_U = '".$sesion."'");
                 if(mysql_num_rows($consulta)==0){
-                    echo '<script>alert("No sos Administrador");</script>';
+                    echo '<script>alert('.$mensaje.');</script>';
                     echo '<script>window.location="../index.php";</script>';
                 }
             }else{
                 if($rol == "asesor"){
                     $consulta =$conectar->consulta("select * from asesor where NOMBRE_U = '".$sesion."'");
                     if(mysql_num_rows($consulta)==0){
-                        echo '<script>alert("No sos Asesor");</script>';
+                        echo '<script>alert('.$mensaje.');</script>';
                         echo '<script>window.location="../index.php";</script>';
                     }
             
@@ -29,7 +30,7 @@
                     if($rol == "grupoEmpresa"){
                         $consulta =$conectar->consulta("select * from grupo_empresa where NOMBRE_U = '".$sesion."'");
                         if(mysql_num_rows($consulta)==0){
-                            echo '<script>alert("No sos una Grupo Empresa");</script>';
+                            echo '<script>alert('.$mensaje.');</script>';
                             echo '<script>window.location="../index.php";</script>';
                         }
                     }
