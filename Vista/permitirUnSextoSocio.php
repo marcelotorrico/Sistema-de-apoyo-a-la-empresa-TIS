c1f74d76cca37cf415ae33d7d4674b46c353860f
Fernando Gutierrez <fernandogutierrez279@gmail.com>
2015-05-13 18:10:53 -0400

    correccion

--- Vista/permitirUnSextoSocio.php
+++ Vista/permitirUnSextoSocio.php
@@ -6,18 +6,20 @@
   $empresa = $_GET['GE'];
  
 
-  $peticion = $conexion->consulta("SELECT NUM_SOCIOS as cantidad FROM grupo_empresa WHERE NOMBRE_U = '$empresa' ");
+  $peticion = $conexion->consulta("SELECT NUM_SOCIOS as cantidad FROM grupo_empresa WHERE NOMBRE_U = '$empresa'");
   $cant = mysql_fetch_array($peticion);
   $resultado= $cant['cantidad'];
+
       
       if ($resultado < 6) {
       	
       	$conexion->consulta("UPDATE grupo_empresa SET NUM_SOCIOS=6 WHERE  NOMBRE_U='$empresa'");
-        $mensaje ="Se concedio el permiso para registrar un sexto socio a $empresa";
-        echo"<script>alert($mensaje); window.location='../Vista/AdministrarGrupoEmpresa.php';</script>";
+        echo"<script>alert('Se concedio el permiso para registrar un sexto socio'); window.location='../Vista/AdministrarGrupoEmpresa.php';</script>";
+  
       } else {
-        $mensaje = "$empresa ya tiene permiso para registrar un sexto socio";
-         echo"<script>alert($mensaje); window.location='../Vista/AdministrarGrupoEmpresa.php';</script>";           	
+        
+         echo"<script>alert('La grupo empresa ya cuenta con el permiso para registrar un sexto socio'); window.location='../Vista/AdministrarGrupoEmpresa.php';</script>";           	
+         
       }
-      
+    
  ?>
