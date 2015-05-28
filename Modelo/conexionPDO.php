3474b49d934c7b7d9fc4e0a78d2ac00ee57882cb
Teo Panda <antonio@publiaciertos.net>
2015-05-27 21:27:05 -0400

    conexionPDO

--- Modelo/conexionPDO.php
+++ Modelo/conexionPDO.php
@@ -1,47 +1,22 @@
-<?php  
-class conexion{
-	public $conn;
-
-	function __construct(){
-	    
-	    $this->conectar();
-	    
-	}	
- 
- public function conectar()
- {
- 	global $conn;
- 	try{
-
-	 $conn = new PDO('mysql:host=localhost;dbname=saetis','root','');
-
-	 $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-	
-
-	}catch(PDOException $e){
-
-	    echo "ERROR: " . $e->getMessage();
-    
-	}
-	
-   }
-   public function consulta($consulta)
-   {
-   	global $conn;
-   	  $sql = $conn->prepare($consulta);
-	  $sql->execute();
-	  $resultado = $sql->fetchAll();
-	  return $resultado;
-   }
-   public function consultaP($consulta,$arrayy)
-   {
-   	global $conn;
-   	  $sql = $conn->prepare($consulta);
-	  $sql->execute($arrayy);
-	  $resultado = $sql->fetchAll();
-	  return $resultado;
-   }
+<?php 
+/* 
+ * Author Antonio
+ */
+ class Conexion extends PDO { 
+   private $tipo_de_base = 'mysql';
+   private $host = 'localhost';
+   private $nombre_de_base = 'saetis';
+   private $usuario = 'root';
+   private $contrasena = ''; 
    
- }
-  
+   public function __construct() {
+      //Sobreescribo el método constructor de la clase PDO.
+      try{
+         parent::__construct($this->tipo_de_base.':host='.$this->host.';dbname='.$this->nombre_de_base, $this->usuario, $this->contrasena);
+      }catch(PDOException $e){
+         echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
+         exit;
+      }
+   }
+ } 
 ?>
