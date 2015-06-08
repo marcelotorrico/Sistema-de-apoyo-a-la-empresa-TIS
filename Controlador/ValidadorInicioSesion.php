8e8dcf8411ca471e6710bb006c44c9f219e3686f
Fernando Gutierrez <fernandogutierrez279@gmail.com>
2015-06-08 08:25:47 -0400

    Arreglado el error de la conexion redeclarada

--- Controlador/ValidadorInicioSesion.php
+++ Controlador/ValidadorInicioSesion.php
@@ -1,40 +1,14 @@
- <?php
- 
- class ValidadorInicioSesion {
-    function validarInicioSesion($sesion,$rol){
-        include_once '../Modelo/conexionPDO.php';
-         $conectar = new Conexion();
-         $user = isset($sesion);
- 
-         if($user == ''){
-             
-             echo '<script>alert("Primero debe iniciar sesion para acceder al sitio");</script>';
-             echo '<script>window.location="../index.php";</script>';
-        }else{
-            if($rol == "administrador"){
-                $consulta =$conectar->query("select * from administrador where NOMBRE_U = '".$sesion."'");
-                if($consulta->rowCount()==0){
-                    echo '<script>alert("Usted intento ingresar a una pagina que no le corresponde");</script>';
-                    echo '<script>window.location="../index.php";</script>';
-                }
-            }else{
-                if($rol == "asesor"){
-                    $consulta =$conectar->query("select * from asesor where NOMBRE_U = '".$sesion."'");
-                    if($consulta->rowCount()==0){
-                        echo '<script>alert("Usted intento ingresar a una pagina que no le corresponde");</script>';
-                        echo '<script>window.location="../index.php";</script>';
-                    }
-            
-                }else{
-                    if($rol == "grupoEmpresa"){
-                        $consulta =$conectar->query("select * from grupo_empresa where NOMBRE_U = '".$sesion."'");
-                        if($consulta->rowCount()==0){
-                            echo '<script>alert("Usted intento ingresar a una pagina que no le corresponde");</script>';
-                            echo '<script>window.location="../index.php";</script>';
-                        }
-                    }
-                }
+ <?php 
+  class ValidadorInicioSesion {
+     function validarInicioSesion($ses,$rol){
+        
+            $sesion = $_SESSION['sesion'];
+
+            if (strcmp ($sesion, $rol ) != 0) {
+                echo '<script>alert("Usted intento ingresar a una pagina que no le corresponde");</script>';
+                echo '<script>window.location="../index.php";</script>';
             }
-         }
-     }
- }
+          
+   }
+}
+?>
