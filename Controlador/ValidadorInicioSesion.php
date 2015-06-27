ffd4c58f56d43ecd0cfe8cdb1c35515b7bf257d8
Fernando Gutierrez <fernandogutierrez279@gmail.com>
2015-06-27 16:59:26 -0400

    Controlar el tiempo de inactividad

--- Controlador/ValidadorInicioSesion.php
+++ Controlador/ValidadorInicioSesion.php
@@ -7,7 +7,43 @@
             if (strcmp ($sesion, $rol ) != 0) {
                 echo '<script>alert("Usted intento ingresar a una pagina que no le corresponde");</script>';
                 echo '<script>window.location="../index.php";</script>';
-            }       
+            }
+
+	//Expire the session if user is inactive for 2
+	//minutes or more.
+	$expireAfter = 1;
+
+
+
+	//Check to see if our "last action" session
+	//variable has been set.
+	if(isset($_SESSION['last_action'])){
+	    
+	    //Figure out how many seconds have passed
+	    //since the user was last active.
+	    $secondsInactive = time() - $_SESSION['last_action'];
+	    
+	    //Convert our minutes into seconds.
+	    $expireAfterSeconds = $expireAfter * 60;
+	    
+	    //Check to see if they have been inactive for too long.
+	    if($secondsInactive >= $expireAfterSeconds){
+	        //User has been inactive for too long.
+	        //Kill their session.
+	        session_unset();
+	        session_destroy();
+	        echo "<script> alert('La sesion caduco por inactividad, por favor inicie sesion nuevamente');window.location='../index.php';</script>";
+           
+	    }
+	    
+	}
+	//Assign the current timestamp as the user's
+	//latest activity
+	$_SESSION['last_action'] = time();
+
+	
+
+	      
    }
 }
 ?>
